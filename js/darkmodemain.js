a8243bb9a91d20dffccded45aabe3f0abf48dfc6
keyraweez <64283899+keyraweez@users.noreply.github.com>
2020-05-20 10:49:25 +0300

    Add files via upload

--- js/darkmodemain.js
+++ js/darkmodemain.js
@@ -2,10 +2,10 @@ document.getElementById('dark').style.opacity = '0';
 function light(){
     document.getElementById('dark').style.opacity = '1';
     document.getElementById('light').style.opacity = '0';
-    document.body.style.background= 'black';
+    document.body.style.background= '#e4ecff';
 }
 function dark(){
     document.getElementById('dark').style.opacity = '0';
     document.getElementById('light').style.opacity = '1';
-    document.body.style.background= 'green';
+    document.body.style.background= '#222';
 }
--- js/test.js
+++ js/test.js
@@ -2,10 +2,12 @@ document.getElementById('dark').style.opacity = '0';
 function light(){
     document.getElementById('dark').style.opacity = '1';
     document.getElementById('light').style.opacity = '0';
+    document.getElementById('dark').style.transition = '.7s';
     document.body.style.background= 'black';
 }
 function dark(){
     document.getElementById('dark').style.opacity = '0';
     document.getElementById('light').style.opacity = '1';
+    document.getElementById('light').style.transition = '.7s';
     document.body.style.background= 'green';
 }
